600314ae954a93a0e1d64017b54987f2adfca2bb
Pedro Sudario <pedrohesudario@hotmail.com>
2025-05-21 19:06:43 -0300

    Initial Commit

---
+++ playlist_babilonica/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  playlist_babilonica
+//
+//  Created by Pedro Henrique Sudario da Silva on 21/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ playlist_babilonica/playlist_babilonicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  playlist_babilonicaApp.swift
+//  playlist_babilonica
+//
+//  Created by Pedro Henrique Sudario da Silva on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct playlist_babilonicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
